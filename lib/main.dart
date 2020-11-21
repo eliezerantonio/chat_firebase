77b8da1c1ee7b895a077900eb4eabe3b055fa628
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2020-11-21 08:54:29 +0100

    dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat/ui/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.blue)
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
---
+++ lib/ui/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:chat/ui/text_composer.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Olá"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: TextComposer(),
+    );
+  }
+}
+
